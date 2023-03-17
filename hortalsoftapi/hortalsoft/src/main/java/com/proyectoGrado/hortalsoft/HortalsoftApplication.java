48fc87315804391d14372da04c044a3b50444172
isabella <you@example.com>
2023-03-17 17:10:00 -0500

    Estructura

---
+++ hortalsoftapi/hortalsoft/src/main/java/com/proyectoGrado/hortalsoft/HortalsoftApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoGrado.hortalsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortalsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HortalsoftApplication.class, args);
+	}
+
+}
